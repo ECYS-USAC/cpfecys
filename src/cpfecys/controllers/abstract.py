a5ffe74ae8c5cd11402b54e4006d887b372bc9b7
Omar Vides <omarvides@gmail.com>
2013-10-28 20:19:12 -0600

    absctract returns name now

--- src/cpfecys/controllers/abstract.py
+++ src/cpfecys/controllers/abstract.py
@@ -5,10 +5,12 @@ def index(): return dict(message="hello from abstract.py")
 def user_active():
     uid = request.vars['uid']
     carnet = None
+    nombre = None
     if uid is None:
         success = False
     else:
         success = True
         carnet = db2(db2.user_user.id ==uid).select().first().username
+        nombre = db(db.auth_user.carnet == carnet).select().first().first_name
 
-    return dict(success=success, carnet = carnet)
+    return dict(success=success, carnet = carnet, name = nombre)
