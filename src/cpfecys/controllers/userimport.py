8293aaf3691d296836b1f7b4a1bc4aa17f56bab0
Omar Vides <omarvides@gmail.com>
2013-11-06 00:00:34 -0600

    users via csv

--- src/cpfecys/controllers/userimport.py
+++ src/cpfecys/controllers/userimport.py
@@ -2,12 +2,29 @@
 # try something like
 import csv
 @auth.requires_login()
-def loadcsv():
-    string = ''
+def index():
+    newUsrs = {}
+    errUsrs = {}
+    UsrIndx = 0
+    errIndx = 0
+    success = False
     if request.vars.csvfile != None:
         file = request.vars.csvfile.file
+        cr = csv.reader(file, delimiter=',', quotechar='|')
+        success = True
+        
+        for row in cr:    
+            user = None
+            user = db2(db2.user_user.username==row[0]).select().first()
+            
+            if user is None:
+                newUsrs[UsrIndx] = row[0]
+                UsrIndx = UsrIndx + 1
+            else:
+                errUsrs[errIndx] = row[0]
+                errIndx = errIndx + 1
+        
         response.flash = T('Data uploaded')
-        content = file.read()
-        return dict(file=content)
+        return dict(success=success, data=newUsrs, errors=errUsrs)
     else:
-        return dict(file=False)
+        return dict(success=False, file=False)
