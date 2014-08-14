91ff643830459284739f1cb523da4da41a028dc6
wilfuentes005 <wil.fuentes005@gmail.com>
2014-08-13 21:12:16 -0600

    Fix date of items and fix the email
    Fix the date of the items and replace the \n in the notification for
    <br>

--- src/cpfecys/controllers/notification.py
+++ src/cpfecys/controllers/notification.py
@@ -200,6 +200,7 @@ def teacher_send_mail_to_students(users1, users2, message, subject, check, semes
     except:
         None
     period = T(semester)+' '+str(year)
+    message = message.replace("\n","<br>")
     messageC = '<html>' + message + attachment_m + '<br><br>'+str(nameS2)+'<br>'+str(period)+'<br>'+str(nameP)+'<br>Sistema de Control de Estudiantes de Practica Final<br> Escuela de Ciencias y Sistemas - Universidad de San Carlos de Guatemala</html>'
     #variable de control
     control = 0
@@ -774,6 +775,7 @@ def send_mail_to_students(users, message, subject, check, semester, year):
     except:
         None
     period = T(semester)+' '+str(year)
+    message = message.replace("\n","<br>")
     messageC = '<html>' + message + attachment_m + '<br><br>'+str(nameS2)+'<br>'+str(period)+'<br>'+str(nameP)+'<br>Sistema de Control de Estudiantes de Practica Final<br> Escuela de Ciencias y Sistemas - Universidad de San Carlos de Guatemala</html>'
     #variable de control
     control = 0
