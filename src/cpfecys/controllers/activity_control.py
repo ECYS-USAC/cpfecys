356c70180479f9e3b0e9c42da9aeee1ff039543e
lecatavo <lecatavo@gmail.com>
2014-09-09 15:02:51 -0600

    Auth user weighting verification

--- src/cpfecys/controllers/activity_control.py
+++ src/cpfecys/controllers/activity_control.py
@@ -156,102 +156,86 @@ def admin_courses_list():
 @auth.requires(auth.has_membership('Student') or auth.has_membership('Teacher') or auth.has_membership('Super-Administrator'))
 def students_control():
     import cpfecys
-    #Obtener la asignacion del estudiante
-    assignation = request.vars['assignation']
-    #Obtener al tutor del proyecto
     year = cpfecys.current_year_period().id
-    check = db.user_project(id = assignation, assigned_user = auth.user.id)
-    if (check is None):
-        #check if there is no assignation or if it is locked (shouldn't be touched)
-        if (session.last_assignation is None):
-            return dict(project = request.vars['project'], year = year)
-        else:
-            check = db.user_project(id = session.last_assignation)
-            if cpfecys.assignation_is_locked(check):
+    project_var = request.vars['project']
+    if auth.has_membership('Super-Administrator') == False:
+        assigantion = db((db.user_project.assigned_user == auth.user.id) & (db.user_project.period == year) & (db.user_project.project == project_var)).select().first()
+        
+        if assigantion is None:
+            academic_var = db(db.academic.carnet==auth.user.username).select().first()
+            try:
+                academic_assig = db((db.academic_course_assignation.carnet == academic_var.id) & (db.academic_course_assignation.semester == year) & (db.academic_course_assignation.assignation==project_var) ).select().first()
+            
+                if academic_assig is None:
+                    session.flash=T('You do not have permission to view course requests')
+                    redirect(URL('default','index'))
+            except:
+                session.flash=T('You do not have permission to view course requests')
                 redirect(URL('default','index'))
-                return
-    else:
-        session.last_assignation = check.id
-
-    #year = db.period_year(id=check.period)
-    #year_semester = db.period(id=year.period)
-
-    return dict(project = request.vars['project'], year = year)
+        
+    return dict(project = project_var, year = year)
 
 @auth.requires_login()
 def control_weighting():
     import cpfecys
-    #Obtener la asignacion del estudiante
-    assignation = request.vars['assignation']
-    
     year = db(db.period_year.id == request.vars['year']).select().first() 
     year_semester = year.period
-    
-    if assignation == 'None':
-        check = db(db.project.id == request.vars['project']).select().first()
-        return dict(name = check.name,
-            semester = year_semester.name,
-            year = year.yearp,
-            assignation=assignation,
-            semestre2 = year,
-            project = request.vars['project'])
-    else:
-        check = db.user_project(id = assignation, assigned_user = auth.user.id)
-        if (check is None):
-            #check if there is no assignation or if it is locked (shouldn't be touched)
-            if (session.last_assignation is None):
-                redirect(URL('default','index'))
-                return
-            else:
-                check = db.user_project(id = session.last_assignation)
-                if cpfecys.assignation_is_locked(check):
-                    redirect(URL('default','index'))
-                    return
-        else:
-            session.last_assignation = check.id
-        
-        return dict(name = check.project.name,
-                    semester = year_semester.name,
-                    year = year.yearp,
-                    assignation=assignation,
-                    semestre2 = year)
 
+    assigantion = db((db.user_project.assigned_user == auth.user.id) & (db.user_project.period == year.id) & (db.user_project.project == request.vars['project'])).select().first()
+    if assigantion is None:
+        assigned_to_project = False
+    else:
+        assigned_to_project = True
+
+    check = db(db.project.id == request.vars['project']).select().first()
+    return dict(name = check.name,
+        semester = year_semester.name,
+        year = year.yearp,
+        semestre2 = year,
+        project = request.vars['project'],
+        assigned_to_project = assigned_to_project)
+    
 @auth.requires_login()
 def students_control_full():
     import cpfecys
-    #Obtener la asignacion del estudiante
-    assignation = request.vars['assignation']
     project = request.vars['project']
     
     year = db(db.period_year.id == request.vars['year']).select().first() 
+
+    project_var = request.vars['project']
+    if auth.has_membership('Super-Administrator') == False:
+        assigantion = db((db.user_project.assigned_user == auth.user.id) & (db.user_project.period == year.id) & (db.user_project.project == project_var)).select().first()
+        
+        if assigantion is None:
+            academic_var = db(db.academic.carnet==auth.user.username).select().first()
+            try:
+                academic_assig = db((db.academic_course_assignation.carnet == academic_var.id) & (db.academic_course_assignation.semester == year.id) & (db.academic_course_assignation.assignation==project_var) ).select().first()
+            
+                if academic_assig is None:
+                    session.flash=T('You do not have permission to view course requests')
+                    redirect(URL('default','index'))
+            except:
+                session.flash=T('You do not have permission to view course requests')
+                redirect(URL('default','index'))
     
-    
+
     return dict(name = '',
                 semester = year.period.name,
-                year = year.yearp,
-                assignation=assignation)
+                year = year.yearp)
 
 @auth.requires_login()
 def control_students_modals():
     import cpfecys
-    #Obtener la asignacion del estudiante
-    assignation = request.vars['assignation']
     project = request.vars['project']
     
     year = db(db.period_year.id == request.vars['year']).select().first() 
     
-    if (assignation == 'None'):
-        project_var = db(db.project.id == request.vars['project']).select().first() 
-        return dict(semestre2 = year, name=project_var.name)
-    else:
-        check = db.user_project(id = assignation, assigned_user = auth.user.id)
-        return dict(semestre2 = year, name=check.project.name)
+    project_var = db(db.project.id == request.vars['project']).select().first() 
+    return dict(semestre2 = year, name=project_var.name)
 
 @auth.requires_login()
 def weighting():
     import cpfecys
-    #Obtener la asignacion del estudiante
-    assignation = request.vars['assignation']
     project = request.vars['project']
    
     year = db(db.period_year.id == request.vars['year']).select().first() 
