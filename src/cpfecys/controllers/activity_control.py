2e963e25ff57cf83c4c8d77958481b1760fc0c8d
wilfuentes005 <wil.fuentes005@gmail.com>
2014-10-16 18:28:00 -0600

    Fix Export of General Report
    Fix Export of General Report

--- src/cpfecys/controllers/activity_control.py
+++ src/cpfecys/controllers/activity_control.py
@@ -1825,7 +1825,8 @@ def general_report_activities_export():
     catLabTemp=None
     catVecLabTemp=[]
     LabActivities = None
-    if existLab == True:
+    validateLaboratory=None
+    if existLab == True or request.vars['type'] == 'lab':
         validateLaboratory = db((db.validate_laboratory.semester==year.id)&(db.validate_laboratory.project==project_var.id)).select()
         LabCategory = db((db.course_activity_category.semester==year.id)&(db.course_activity_category.assignation==project_var.id)&(db.course_activity_category.laboratory==True)).select()
         LabActivities = []
